651dca5731d6af1d8d880c0d96bc290f56d7d415
Meitar S <44778545+Meitie@users.noreply.github.com>
2020-11-05 12:14:47 +0200

    adding delete

--- deletion/delete.py
+++ deletion/delete.py
@@ -1,1 +1,7 @@
-a
+
+def delete_event(service, eventsID):
+    """
+    Takes in the event's ID in dictionary form "individual_time[0]['id']" as an argument, and then deletes that from the calendar
+    """
+    return service.events().delete(calendarId='primary',
+                                   eventId=eventsID).execute()
