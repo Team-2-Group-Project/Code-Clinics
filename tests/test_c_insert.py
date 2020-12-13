5d144a16edb81fe3a339d2f0d5c09b28d25dafec
Byronitus <70758340+Byronitus@users.noreply.github.com>
2020-12-13 12:58:02 +0200

    Update test_c_insert.py

--- tests/test_c_insert.py
+++ tests/test_c_insert.py
@@ -1,30 +1,45 @@
 import unittest
-import insert
 from unittest.mock import patch
-import io
+import io, os
 import sys
+USER_PATHS = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '../'))
+sys.path.insert(0, USER_PATHS + "/")
+import clinician.insert as insert
+
 
 class Test_Insert(unittest.TestCase):
+
+    text = io.StringIO()
+    sys.stdout = text
 
     def test_valid_date(self):
         result1 = insert.valid_date('2020-11-20')
         result2 = insert.valid_date('2002020-123-200')
-        self.assertEqual(result1,'2020-11-20')
-        self.assertEqual(result2,'')
+        self.assertEqual(result1,True)
+        self.assertEqual(result2,False)
 
     def test_valid_time(self):
         result1 = insert.valid_time('12:05')
         result2 = insert.valid_time('132:102')
-        self.assertEqual(result1,'12:05')
-        self.assertEqual(result2,'')
+        self.assertEqual(result1,True)
+        self.assertEqual(result2,False)
 
-    @patch('sys.stdin',io.StringIO('2020-11-207\n2020-15-36\n15:36\n2020-12-10\n25:26\n2020-12-10\n12:30'))
-    def test_valid_date_checker(self):
-        text = io.StringIO()
-        sys.stdout = text
-        result1 = insert.valid_date_checker()
-        self.assertEqual(result1,('2020-12-10T12:30:00+02:00', '2020-12-10T13:00:00+02:00'))
+    def test_validate_params(self):
+        result1 = insert.validate_params([])
+        result2 = insert.validate_params(['','',''])
+        result3 = insert.validate_params(['2020-13-20','14:30'])
+        self.assertEqual(result1,('',''))
+        self.assertEqual(result2,('',''))
+        self.assertEqual(result3,('2020-13-20','14:30'))
+
+    
+    def test_meetings_setups(self):
+        result1 = insert.meeting_setups([],'John')
+        result2 = insert.meeting_setups(['TR5','Help'],'John')
+        self.assertEqual(result1,('John','Open for anything'))
+        self.assertEqual(result2,('TR5','Help'))
 
 
 if __name__ == '__main__':
     unittest.main()
+    
